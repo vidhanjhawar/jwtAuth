82336661ed558bebd3b3ba484580b452735bb25c
Vidhan Jhawar (Residents) <vidhan.jhawar98@gmail.com>
2019-02-09 15:45:09 +0530

    initial comit

--- src/main/java/jwtGenerator/JwtApi.java
+++ src/main/java/jwtGenerator/JwtApi.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class jwtGeneratorApi {
+public class JwtApi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SpringApplication.run(jwtGeneratorApi.class, args);
+		SpringApplication.run(JwtApi.class, args);
 	}
 
 }
--- src/main/java/jwtGenerator/jwtController.java
+++ src/main/java/jwtGenerator/jwtController.java
@@ -1,7 +1,6 @@
 package jwtGenerator;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
